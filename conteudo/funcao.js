a84f5601bc3d0b254e3d4f1bd4495d0b29dcf26c
wendell <123216078+wendell-S@users.noreply.github.com>
2023-07-06 21:06:53 -0300

    V0.0.4
    correçao diviçao

--- conteudo/funcao.js
+++ conteudo/funcao.js
@@ -20,13 +20,13 @@ function somarvezes () {
     res_v.innerHTML = `A soma de ${n1_v} vezes ${n2_v} resulta em ${s}`
 }
 
-function somarvezes () {  //DIVISAO
-    var n1_v = window.document.getElementById('n1v')
-    var n2_v = window.document.getElementById('n2v')
-    var res_v = window.document.getElementById('res_v')
-    var n1_v = Number (n1_v.value)
-    var n2_v = Number (n2_v.value)
-    var s = n1_v * n2_v
+function somardiv () {  //DIVISAO
+    var n1_d = window.document.getElementById('n1div')
+    var n2_d = window.document.getElementById('n2div')
+    var res_div = window.document.getElementById('res_div')
+    var n1_div = Number (n1_d.value)
+    var n2_div = Number (n2_d.value)
+    var s = n1_div / n2_div
 
-    res_v.innerHTML = `A soma de ${n1_v} vezes ${n2_v} resulta em ${s}`
+    res_div.innerHTML = `A soma de ${n1_div} vezes ${n2_div} resulta em ${s}`
 }
